7a5656cf124bb0199c486c124667e2eaab5dc0b1
Arian van Putten <aeroboy94@gmail.com>
2016-04-28 17:13:53 +0200

    State is overkill for keeping the current value

--- src/CRDT/AntiEntropy.hs
+++ src/CRDT/AntiEntropy.hs
@@ -2,28 +2,30 @@
 module CRDT.AntiEntropy where
 import Algebra.Lattice
 import Control.Monad.Writer.Class
-import Control.Monad.State.Class
 import Control.Monad.Random.Class
 
 
 
 
 -- Chooses to return a delta or the full state
-choose :: ( MonadRandom m
-          , BoundedJoinSemiLattice w
-          , MonadState w m
-          , MonadWriter (Join w) m
-          ) => m w
-choose = do
-  x <- getRandom
+choose' :: ( BoundedJoinSemiLattice w
+           , MonadWriter (Join w) m
+           ) =>  w  -> Bool -> m w
+choose' w0 x = do
   w <-
     if x
       then do 
         (_,Join w) <- listen (return ())
         return w
-      else get
+      else return w0
   censor (const mempty) (return w)
-      
+  
+
+choose :: ( MonadRandom m
+          , BoundedJoinSemiLattice w
+          , MonadWriter (Join w) m
+          ) => w -> m w
+choose w = getRandom >>= choose' w
 
 {-
 antiEntropyCtr :: ( MonadRandom m
