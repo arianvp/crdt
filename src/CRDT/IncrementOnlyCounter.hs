d7fe52ac3e507dfe3be302df7fec4fb0a9bf5a5a
Arian van Putten <aeroboy94@gmail.com>
2016-04-28 16:59:55 +0200

    Parameterize the counter index

--- src/CRDT/IncrementOnlyCounter.hs
+++ src/CRDT/IncrementOnlyCounter.hs
@@ -1,46 +1,51 @@
+{-#LANGUAGE MultiParamTypeClasses #-} 
+{-#LANGUAGE FlexibleContexts #-}
 module CRDT.IncrementOnlyCounter where
 import Algebra.Lattice
 import qualified Data.Map as Map
 import Data.Map ((!))
 import qualified Data.Set as Set
 import qualified Data.Foldable as Foldable
+import Control.Monad.Writer.Class
 
 
 -- The increment-only counter assumes that we have a fixed network size, and a unique id
 -- within the counter.
 --
 
-newtype IncrementOnlyCounter = IncrementOnlyCounter (Map.Map Int Int)
+newtype IncrementOnlyCounter a = IncrementOnlyCounter (Map.Map a Int)
 
-instance Show IncrementOnlyCounter where
+instance Ord a => Show (IncrementOnlyCounter a) where
   show x = "IncrementOnlyCounter " ++ show (value x)
 
-instance JoinSemiLattice IncrementOnlyCounter where
+instance Ord a => JoinSemiLattice (IncrementOnlyCounter a) where
   (IncrementOnlyCounter v1) \/ (IncrementOnlyCounter v2) = 
     IncrementOnlyCounter (Map.unionWith max v1 v2)
 
 
-instance BoundedJoinSemiLattice IncrementOnlyCounter where
+instance Ord a => BoundedJoinSemiLattice (IncrementOnlyCounter a) where
   bottom = IncrementOnlyCounter Map.empty 
 
-
-local :: Int -> IncrementOnlyCounter -> Int
+local :: Ord a => a -> IncrementOnlyCounter a -> Int
 local i (IncrementOnlyCounter m) = m ! i
 
-value :: IncrementOnlyCounter -> Int
+value :: Ord a => IncrementOnlyCounter a -> Int
 value (IncrementOnlyCounter v) = Map.foldr (+) 0 v
 
-increment :: Int -> IncrementOnlyCounter -> IncrementOnlyCounter
+increment :: Ord a => a -> IncrementOnlyCounter a -> IncrementOnlyCounter a
 increment myIndex (IncrementOnlyCounter v) =
   IncrementOnlyCounter (Map.insertWith (\a b -> a + b + 1) myIndex 1 v)
 
+incrementWithDelta :: (Ord a, MonadWriter (IncrementOnlyCounter a) m) => a -> IncrementOnlyCounter a -> m (IncrementOnlyCounter a)
+incrementWithDelta i c = do
+  let newCounter = increment i c
+  tell (IncrementOnlyCounter (Map.singleton i (local i newCounter)))
+  return newCounter
 
--- TODO: use state monad
-incrementWithDelta :: Int -> IncrementOnlyCounter -> (IncrementOnlyCounter, IncrementOnlyCounter)
-
-incrementWithDelta i c =
+{-incrementWithDelta i c =
   let newCounter = increment i c
   in (newCounter, IncrementOnlyCounter (Map.singleton i (local i newCounter)))
 
+-}
 
 
--- src/CRDT/PNCounter.hs
+++ src/CRDT/PNCounter.hs
@@ -3,40 +3,42 @@ import Algebra.Lattice
 import qualified CRDT.IncrementOnlyCounter as GCounter
 
 
-data PNCounter = PNCounter
-  { p :: GCounter.IncrementOnlyCounter
-  , n :: GCounter.IncrementOnlyCounter
+data PNCounter a = PNCounter
+  { p :: GCounter.IncrementOnlyCounter a 
+  , n :: GCounter.IncrementOnlyCounter a
   }
 
+instance Ord a => Show (PNCounter a) where
+  show x = "PNCounter " ++ show (value x)
 
-instance JoinSemiLattice PNCounter where
+instance Ord a => JoinSemiLattice (PNCounter a) where
   (PNCounter p1 n1) \/ (PNCounter p2 n2) = PNCounter (p1 \/ p2) (n1 \/ n2)
     
-instance BoundedJoinSemiLattice PNCounter where
+instance Ord a => BoundedJoinSemiLattice (PNCounter a) where
   bottom = PNCounter bottom bottom
 
 
-local :: Int -> PNCounter -> Int
+local :: Ord a => a -> PNCounter a -> Int
 local i (PNCounter p n) =
   GCounter.local i p - GCounter.local i n
 
 
-value :: PNCounter -> Int
+value :: Ord a => PNCounter a -> Int
 value (PNCounter p n) = GCounter.value p - GCounter.value n
 
-increment :: Int -> PNCounter -> PNCounter
+increment :: Ord a => a -> PNCounter a -> PNCounter a
 increment i (PNCounter p n) = PNCounter (GCounter.increment i p) n
 
-incrementWithDelta :: Int -> PNCounter -> (PNCounter, PNCounter)
+incrementWithDelta :: Ord a => a -> PNCounter a -> (PNCounter a , PNCounter a)
 incrementWithDelta i pn =
   let newValue = increment i pn
   in (newValue, bottom { p = p newValue })
 
 
-decrement :: Int -> PNCounter -> PNCounter
+decrement :: Ord a => a -> PNCounter a  -> PNCounter a
 decrement i (PNCounter p n) = PNCounter p (GCounter.increment i n)
 
-decrementWithDelta :: Int -> PNCounter -> (PNCounter, PNCounter)
+decrementWithDelta :: Ord a => a -> PNCounter a -> (PNCounter a, PNCounter a)
 decrementWithDelta i pn =
   let newValue = decrement i pn
   in (newValue, bottom { n = n newValue })
